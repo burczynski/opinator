7de29bbf44f7a1741bcf3c644a4dc3fac7c42ece
burczynski <burczynski_ricardohhotmail.com>
2018-12-02 20:38:32 -0300

    server.js actualizado con rutas de prueba

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-const http     = require('http');
-const hostname = '127.0.0.1';
-const port     = 3000;
+var express        = require("express"),
+    app            = express(),
+    bodyParser     = require("body-parser"),
+    methodOverride = require("method-override");
+    //mongoose       = require('mongoose');
 
-const server   = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
 
-server.listen(port, hostname, () => {
-  console.log(`Servidor corriendo en http://${hostname}:${port}/`);
+var router = express.Router();
+
+router.route('/')
+	.get( (req, res)=> {})
+	/*.post()*/;
+
+router.route('/quierotodo')
+	.get( (req, res)=> {
+		res.send('vete al diablo, idiota');
+	});
+
+app.use(router);
+
+app.listen(3000, ()=> {
+  console.log("Node server running on http://localhost:3000");
 });
