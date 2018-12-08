cc6c9edda63a88f9ca5d8d7c3a934fdb03fd1471
burczynski <burczynski_ricardohhotmail.com>
2018-12-08 18:43:23 -0300

    added origin all and new file with ajax request

--- server.js
+++ server.js
@@ -10,13 +10,37 @@ app.use(methodOverride());
 
 var router = express.Router();
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 router.route('/')
-	.get( (req, res)=> {})
+	.get( (req, res)=> {
+		res.send('aca va a retornar la informacion general');
+	})
 	/*.post()*/;
 
-router.route('/quierotodo')
+router.route('/newuser')
 	.get( (req, res)=> {
-		res.send('vete al diablo, idiota');
+		res.send('aca va a darse de alta un usuario nuevo');
+	});
+
+router.route('/newpost')
+	.post( (req, res)=> {
+		let obj = {
+			name : 'jijo',
+		};
+		console.log(obj);
+		res.end(JSON.stringify(obj));
+		/*var response = {
+		    status  : 200,
+		    success : 'Updated Successfully'
+		}
+
+		res.end(JSON.stringify(response));*/
 	});
 
 app.use(router);
